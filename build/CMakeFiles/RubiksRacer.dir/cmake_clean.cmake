d5c225add6d0af18337915899dcc43bada3ad41b
Christopher Seidl <2367ChristopherMarkSeidl@gmail.com>
2024-09-21 17:51:26 +1000

    set up project structure

---
+++ build/CMakeFiles/RubiksRacer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RubiksRacer.dir/src/main.cpp.o"
+  "CMakeFiles/RubiksRacer.dir/src/main.cpp.o.d"
+  "RubiksRacer"
+  "RubiksRacer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RubiksRacer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/RubiksRacer.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for RubiksRacer.
+# This may be replaced when dependencies are built.
