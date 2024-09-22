496322deb50a32ef841c05ad74ad9453719a045e
Christopher Seidl <2367ChristopherMarkSeidl@gmail.com>
2024-09-22 14:33:05 +1000

    more code cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -10,58 +10,49 @@
 #include <unordered_set>
 #include <vector>
 
-void scramble_cube(Cube &cube, size_t num_scrambles) {
-  for (size_t i = 0; i < num_scrambles; i++) {
-    CubeFace face = static_cast<CubeFace>(rand() % 6);
-    Rotation rotation = static_cast<Rotation>(rand() % 2);
-    std::cout << " * scrambling: " << face << " " << rotation << std::endl;
-    cube.rotate_face(face, rotation);
-  }
-}
-
-struct CubeSolution {
-  Cube cube;
-  std::vector<std::pair<CubeFace, Rotation>> moves;
-
-  // TODO: refine this heuristic
-  double score() const {
-    double score = 1.0;
-    for (const auto &[_, side] : cube.m_sides_from_faces) {
-      score += 0.1 * side.number_of_matching_rows_and_columns();
-    }
-    return score / std::max(1.0, (double)moves.size());
-  }
-
-  bool operator<(const CubeSolution &other) const {
-    return score() < other.score();
-  }
-};
-
 int main(int argc, char **argv) {
 
   auto start = std::chrono::high_resolution_clock::now();
 
-  std::cout << std::endl;
   Cube starting_cube = Cube::SolvedCube();
 
-  size_t num_scrambles = 20;
-  if (argc >= 2) {
-    num_scrambles = std::stoi(argv[1]);
-  }
-
-  auto seed = time(nullptr);
-  if (argc >= 3) {
-    seed = std::stoi(argv[2]);
-  }
-  std::cout << "seed: " << seed << std::endl;
+  size_t num_scrambles = argc >= 2 ? std::stoi(argv[1]) : 20;
+  auto seed = argc >= 3 ? std::stoi(argv[2]) : time(nullptr);
   srand(seed);
 
   std::cout << std::endl;
+  std::cout << "seed: " << seed << std::endl;
   std::cout << "number of scrambles: " << num_scrambles << std::endl;
-  scramble_cube(starting_cube, num_scrambles);
+  std::cout << std::endl;
+
+  std::cout << "scrambling cube..." << std::endl;
+  for (size_t i = 0; i < num_scrambles; i++) {
+    CubeFace face = static_cast<CubeFace>(rand() % 6);
+    Rotation rotation = static_cast<Rotation>(rand() % 2);
+    std::cout << " * scrambling: " << face << " " << rotation << std::endl;
+    starting_cube.rotate_face(face, rotation);
+  }
   std::cout << std::endl;
 
   std::cout << "scrambled cube:" << starting_cube << std::endl;
+
+  struct CubeSolution {
+    Cube cube;
+    std::vector<std::pair<CubeFace, Rotation>> moves;
+
+    // TODO: refine this heuristic
+    double score() const {
+      double score = 1.0;
+      for (const auto &[_, side] : cube.m_sides_from_faces) {
+        score += 0.1 * side.number_of_matching_rows_and_columns();
+      }
+      return score / std::max(1.0, (double)moves.size());
+    }
+
+    bool operator<(const CubeSolution &other) const {
+      return score() < other.score();
+    }
+  };
 
   std::priority_queue<CubeSolution> cube_solution_queue;
   std::unordered_set<Cube> visited_cubes;
@@ -103,7 +94,7 @@ int main(int argc, char **argv) {
             << std::endl;
   auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(
       std::chrono::high_resolution_clock::now() - start);
-  std::cout << "time taken: " << duration.count() << " ms" << std::endl;
+  std::cout << "total time taken: " << duration.count() << " ms" << std::endl;
   std::cout << "time per cube evaluation: "
             << duration.count() / (double)num_evaluated_cubes << " ms"
             << std::endl;
